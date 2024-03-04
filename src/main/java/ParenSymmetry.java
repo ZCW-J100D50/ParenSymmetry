9843a66ecbfa754e9692465ba4eb94bd11e8c060
Mariyana-Katz <mariqnak21@gmail.com>
2024-03-03 23:46:14 -0500

    Finished

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,16 +1,43 @@
 package src.main.java;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
-        // implement this method
-        return null;
+        int numberOpen = 0;
+        int numberClosed = 0;
+        for(int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                numberOpen++;
+            }
+            if (s.charAt(i) == ')') {
+                numberClosed++;
+            }
+        }
+            if(numberOpen==numberClosed && numberOpen >0)
+                return true;
+            else
+                return false;
+
     }
 
     private void checkFile(String filename) {
         // open file named filename
-
+        try
+        {
+            Scanner s = new Scanner( new File(filename) );
+            while( s.hasNextLine() )
+            {
+                System.out.println( isBalanced(s.nextLine() ));
+            }
+        }
+        catch(IOException e)
+        {	System.out.println( e );
+        }
         // for each line in the file
             // read the line
             // print whether or not the line's parenthesis are balanced
@@ -37,6 +64,13 @@ public class ParenSymmetry {
             trues = ps.isBalanced(strToTest);
         }
         printResult(trues, true);
+
+
+        System.out.println("TestStrings0.txt");
+        ps.checkFile("TestStrings0.txt");
+
+        System.out.println("TestStrings1.txt");
+        ps.checkFile("TestStrings1.txt");
 
     }
 
