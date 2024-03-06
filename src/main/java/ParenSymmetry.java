f88b13d39044d9511314ca6101869a7f80b25b49
deepa-kakade-git <deep_303@yahoo.com>
2024-03-05 19:20:50 -0500

    finished

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,19 +1,46 @@
 package src.main.java;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
-        // implement this method
-        return null;
+        int numberOpen = 0;
+        int numberClosed = 0;
+        for(int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                numberOpen++;
+            }
+            if (s.charAt(i) == ')') {
+                numberClosed++;
+            }
+        }
+        if(numberOpen==numberClosed && numberOpen >0)
+            return true;
+        else
+            return false;
+
     }
 
     private void checkFile(String filename) {
         // open file named filename
-
+        try
+        {
+            Scanner s = new Scanner( new File(filename) );
+            while( s.hasNextLine() )
+            {
+                System.out.println( isBalanced(s.nextLine() ));
+            }
+        }
+        catch(IOException e)
+        {   System.out.println( e );
+        }
         // for each line in the file
-            // read the line
-            // print whether or not the line's parenthesis are balanced
+        // read the line
+        // print whether or not the line's parenthesis are balanced
 
         // CLOSE the file
     }
@@ -38,6 +65,13 @@ public class ParenSymmetry {
         }
         printResult(trues, true);
 
+
+        System.out.println("TestStrings0.txt");
+        ps.checkFile("TestStrings0.txt");
+
+        System.out.println("TestStrings1.txt");
+        ps.checkFile("TestStrings1.txt");
+
     }
 
     private static void printResult(Boolean b0, boolean b) {
@@ -52,3 +86,28 @@ public class ParenSymmetry {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
